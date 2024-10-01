4bd9ba605f370d52a44dc19dc847275e7f780f55
Avi <59033172+AviWind02@users.noreply.github.com>
2024-10-01 00:04:27 -0400

    Add project files.

---
+++ Avi's Trainer/script.cpp
@@ -0,0 +1,20 @@
+
+#include <string>
+#include <ctime>
+#include "Script Hook V/inc/main.h"
+
+void main()
+{	
+
+	while (true)
+	{
+	
+		WAIT(0);
+	}
+}
+
+void ScriptMain()
+{
+	srand(GetTickCount());
+	main();
+}
