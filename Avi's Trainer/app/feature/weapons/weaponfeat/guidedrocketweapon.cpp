80b83430356a2d20b1c1ea3593085b3fa8830d92
Avi gill <gillavi2002@yahoo.com>
2024-11-04 20:14:09 -0500

    pop start

---
+++ Avi's Trainer/app/feature/weapons/weaponfeat/guidedrocketweapon.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "weaponfeatures.h"
+
+namespace app {
+    namespace feature::weapon {
+
+        using namespace g_feature;
+        namespace guidedRocketGun {
+            bool tickGuidedRocketGun;
+
+
+            void tick()
+            {
+                if (tickGuidedRocketGun)
+                {
+
+                }
+
+            }
+
+        }
+    }
+}
